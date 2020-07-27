442548fcfaebba8784d600b07d71170a2959e01b
Vasile Rotaru <vasilerotaru38@gmail.com>
2020-07-27 12:32:44 +0300

    `main.dart` contains only theme data. Home page is separated to another file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,78 +17,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        brightness: Brightness.light,
         primarySwatch: Colors.amber,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Barcode scanner'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> history;
-
-  void _scan() async {
-    var _result = await FlutterBarcodeScanner.scanBarcode(
-        "#ff4297", "Cancel", true, ScanMode.DEFAULT);
-    setState(() {
-      history.add("Barcode: " + _result);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    history = [];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var addInventoryButton = RaisedButton(
-      padding: EdgeInsets.symmetric(horizontal: 100),
-      onPressed: () {
-        _scan();
-      },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Icon(Icons.settings_overscan),
-          Text("Inventariere"),
-        ],
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.amber,
+        visualDensity: VisualDensity.comfortable,
       ),
-      color: Theme.of(context).primaryColorDark,
-    );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-        itemCount: history.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text("${index + 1}. ${history[index]}"),
-        ),
-      ),
-      bottomNavigationBar: addInventoryButton,
+      home: HomePage(title: 'Barcode scanner'),
+
     );
   }
 }
