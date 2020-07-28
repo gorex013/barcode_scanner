4e0aa9c53c78039c220f8f730fa2b4e0b80ec595
Vasile Rotaru <vasileroaru38@gmail.com>
2020-07-28 20:42:51 +0300

    Reusable dialog refactored.

--- lib/scan_dialog.dart
+++ lib/scan_dialog.dart
@@ -1,17 +1,18 @@
+import 'package:barcode_scanner/database_management.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanDialog extends StatefulWidget {
   final title;
-  final barcode;
-  final doneButton;
+  final transactionInsert;
+  final mapperFunction;
 
   static _scan() async {
     return FlutterBarcodeScanner.scanBarcode(
         "#ff4297", "Anulează", true, ScanMode.DEFAULT);
   }
 
-  ScanDialog(this.title, this.doneButton, this.barcode);
+  ScanDialog(this.title, this.transactionInsert, this.mapperFunction);
 
   @override
   State<StatefulWidget> createState() => _ScanDialog();
@@ -21,28 +22,79 @@ class _ScanDialog extends State<ScanDialog> {
   var scanController = TextEditingController();
   var quantityController = TextEditingController();
   var quantityFocusNode = FocusNode();
+  var unregistered = false;
+  var unscanned = true;
 
   @override
   Widget build(BuildContext context) {
-    scanController.text = widget.barcode;
+    if (unscanned) scanController.text = "Apasă pentru a scana barcod";
+    var actionButtons = <Widget>[
+      RaisedButton.icon(
+        onPressed: () async {
+          if (scanController.text == "Apasă pentru a scana") {
+            return;
+          }
+          var barcodeID = await Barcode.query(
+            columns: [Barcode.id],
+            where: '${Barcode.barcode} = \"${scanController.text}\"',
+          );
+          print(barcodeID);
+          barcodeID = barcodeID[0][Barcode.id];
+          widget.transactionInsert(
+              widget.mapperFunction(barcodeID, quantityController.text));
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.done),
+        label: Text("Terminat"),
+      ),
+    ];
+    var registerButton = RaisedButton.icon(
+      onPressed: () async {
+        setState(() {
+          Barcode.insert(
+            {
+              Barcode.barcode:  scanController.text,
+              Barcode.startDate: DateTime.now().toIso8601String()
+            },
+          );
+          unregistered = false;
+          unscanned = false;
+        });
+      },
+      icon: Icon(Icons.add),
+      label: Text("Înregistrează"),
+    );
+    if (unregistered) actionButtons.add(registerButton);
     return AlertDialog(
-      insetPadding: EdgeInsets.only(
-          left: MediaQuery.of(context).size.width / 6,
-          right: MediaQuery.of(context).size.width / 6,
-          top: 30,
-          bottom: 270),
+      insetPadding: EdgeInsets.only(top: 30, bottom: 270),
       title: widget.title,
       content: Column(
         children: <Widget>[
           TextField(
-            decoration: InputDecoration(labelText: "Barcode: "),
+            decoration: InputDecoration(
+                labelText: "Barcode: ",
+                errorText:
+                    (unregistered) ? "Produsul nu este înregistrat" : null),
             controller: scanController,
             enableInteractiveSelection: false,
             showCursor: false,
             onTap: () async {
               var result = await ScanDialog._scan();
-              if (result == '-1') return;
-              scanController.text = result;
+              if (result == '-1') {
+                setState(() {
+                  scanController.text = "Apasă pentru a scana barcod";
+                  unscanned = true;
+                });
+                return;
+              }
+              var queryResult = await Barcode.query(
+                  where: '${Barcode.barcode} = \"$result\"');
+              var _unregistered = queryResult.length == 0;
+              setState(() {
+                scanController.text = result;
+                unregistered = _unregistered;
+                unscanned = false;
+              });
             },
           ),
           TextField(
@@ -57,18 +109,16 @@ class _ScanDialog extends State<ScanDialog> {
             onSubmitted: (text) {
               quantityFocusNode.unfocus();
             },
-            onTap: (){
+            onTap: () {
               FocusScope.of(context).requestFocus(quantityFocusNode);
             },
-            onEditingComplete: (){
+            onEditingComplete: () {
               quantityFocusNode.unfocus();
             },
           ),
         ],
       ),
-      actions: <Widget>[
-        widget.doneButton,
-      ],
+      actions: actionButtons,
     );
   }
 }
